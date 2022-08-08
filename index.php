7e47c55d3fda1f07a1b7b348f82209cdc3883a10
iflorido <iflorido@gmail.com>
2022-08-08 13:59:59 +0200

    Añadidas las entidades y POST para los formularios de Ingreso y Retirada

--- index.php
+++ index.php
@@ -1,39 +1,36 @@
 <?php
-echo "Hello there, this is a PHP Apache container";
-?>
-<?php
-//These are the defined authentication environment in the db service
+require_once('entidades/operacion.php');
+require_once('entidades/smovimientos.php');
+require_once('entidades/conexiones.php');
 
-// The MySQL service named in the docker-compose.yml.
-$host = 'localhost';
-
-// Database use name
-$user = 'optima';
-
-//database user password
-$pass = 'Optima_123456';
-
-// database name
-$mydatabase = 'optima';
-// check the mysql connection status
-
-$conn = new mysqli($host, $user, $pass, $mydatabase);
-
-// select query
-$sql = 'SELECT * FROM movimientos';
-
-if ($result = $conn->query($sql)) {
-    echo "me he conectado correctamente";
-    while ($data = $result->fetch_object()) {
-        $users[] = $data;
+if (isset($_POST['ingreso'])) {
+    
+        $servicioMovimeinto = new ServicioMovimiento();
+        $listaSaldo = $servicioMovimeinto->saldo();
+        if (empty($listaSaldo)) {$listaSaldo = 0;}
+        $saldo = ($listaSaldo) + ($_POST['cantidad']);
+        
+        //$saldo = $_POST['saldo'];
+        
+        $tipo = $_POST['tipo'];
+        $cantidad = $_POST['cantidad'];
+         
+        $concepto = $_POST['concepto'];
+        $movimientos = new Operacion($id,$tipo,$fecha,$cantidad,$saldo,$concepto);
+        $servicioBanco = new ServicioMovimiento();
+        $servicioBanco->ingresar($movimientos);
+        
+        }
+ if (isset($_POST['retirar'])) {
+        $tipo = $_POST['tipo'];
+        $cantidad = $_POST['cantidad'];
+        $servicioMovimeinto = new ServicioMovimiento();
+        $listaSaldo = $servicioMovimeinto->saldo();
+        if (empty($listaSaldo)) {$listaSaldo = 0;}
+        $saldo = ($listaSaldo) + (-$_POST['cantidad']);
+        $concepto = $_POST['concepto'];
+        $movimientos = new Operacion($id,$tipo,$fecha,-$cantidad,$saldo,$concepto);
+        $servicioBanco = new ServicioMovimiento();
+        $servicioBanco->retirar($movimientos);
     }
-}
-
-foreach ($users as $user) {
-    echo "<br>";
-    echo $user->tipo . " " . $user->cantidad;
-    echo "<br>";
-}
-//test
 ?>
-
