9703e12cdf3d6d4f9124f16cd132504c7f4a0fe5
iflorido <iflorido@gmail.com>
2022-08-08 13:41:53 +0200

    Inicio de prueba para Optiva2

--- index_optiva.php
+++ index_optiva.php
@@ -1,7 +1,38 @@
 <?php
+echo "Hello there, this is a PHP Apache container";
+?>
+<?php
+//These are the defined authentication environment in the db service
 
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
- */
+// The MySQL service named in the docker-compose.yml.
+$host = 'localhost';
+
+// Database use name
+$user = 'optima';
+
+//database user password
+$pass = 'Optima_123456';
+
+// database name
+$mydatabase = 'optima';
+// check the mysql connection status
+
+$conn = new mysqli($host, $user, $pass, $mydatabase);
+
+// select query
+$sql = 'SELECT * FROM movimientos';
+
+if ($result = $conn->query($sql)) {
+    echo "me he conectado correctamente";
+    while ($data = $result->fetch_object()) {
+        $users[] = $data;
+    }
+}
+
+foreach ($users as $user) {
+    echo "<br>";
+    echo $user->tipo . " " . $user->cantidad;
+    echo "<br>";
+}
+?>
 
