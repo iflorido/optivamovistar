ba7865692049c37637346f68139cd3b91e3031a8
iflorido <iflorido@gmail.com>
2022-08-08 14:02:38 +0200

    Eliminación de la información de la base de datos local con Docker

--- entidades/config.php
+++ entidades/config.php
@@ -12,15 +12,3 @@ $password = 'Optima_123456';
 $database = 'optima';
 ?>
 
-<?php
-
-//datos para dockers
-
-////$host = 'db';
-// Database use name
-//$usuario = 'optima';
-//database user password
-//$password = 'optima12345';
-//Database name
-//$database = 'optima';
-?>
